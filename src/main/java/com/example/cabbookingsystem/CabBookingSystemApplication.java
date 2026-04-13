757e922a11ccefc6db21bd5edd65f523678f1808
JenkinsUserTest <73050663+JenkinsUserTest@users.noreply.github.com>
2026-04-13 13:59:30 +0530

    Initial commit for Cab Booking System

---
+++ src/main/java/com/example/cabbookingsystem/CabBookingSystemApplication.java
@@ -0,0 +1,11 @@
+package com.example.cabbookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabBookingSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CabBookingSystemApplication.class, args);
+    }
+}
